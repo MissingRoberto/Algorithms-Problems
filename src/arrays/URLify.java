d7e41a97d82f30a5a9711a59c4d89ab24b155200
Roberto Jimenez Sanchez <roberto@de.ibm.com>
2016-08-28 18:32:21 +0200

    Add URLify

---
+++ src/arrays/URLify.java
@@ -0,0 +1,25 @@
+package arrays;
+
+public class URLify {
+	
+	public static void URLify(char[] url, int lenght){
+		int counter = 0;
+		for(int i=lenght-1; i>0; i--){
+			if (url[i]==' '){
+				counter++;
+			}
+		}
+		for(int i=lenght-1; i>0 && counter > 0; i--){
+			if (url[i] == ' '){
+				url[i+counter*2]='0';
+				url[i+counter*2 - 1]='2';
+				url[i+counter*2 - 2]='%';
+				counter--;
+			}else{
+				url[i+counter*2]=url[i];
+				url[i]=' ';
+			}
+		}
+		
+	}
+}
