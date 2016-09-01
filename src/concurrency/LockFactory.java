9e2dac454bad39771716954c0a4a443212b825ca
Roberto Jimenez Sanchez <roberto@de.ibm.com>
2016-09-01 22:19:30 +0200

    Added Lock factory

---
+++ src/concurrency/LockFactory.java
@@ -0,0 +1,60 @@
+package concurrency;
+
+import java.util.ArrayList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockFactory {
+	Lock request = new ReentrantLock();
+	ArrayList<LockNode> locks = new ArrayList<LockNode>();
+	public void initialize(int i){
+		for(int j=0; j<i; i++){
+			locks.add(new LockNode());
+		}
+	}
+	
+	public Lock[] getLocks(String owner){
+		ArrayList<Lock> userLocks = new ArrayList<Lock>();
+		for(LockNode n: locks){
+			if(n.owners.contains(owner)){
+				userLocks.add(n.lock);
+			}
+		}
+		return (Lock[]) userLocks.toArray();
+	}
+	
+	public boolean presale(String owner, int[] requestedLocks){
+		request.lock();
+		// Added requested to the graph
+		for(int requestedLock: requestedLocks){
+			for(int child: requestedLocks){
+				LockNode lock = locks.get(requestedLock);
+				lock.owners.add(owner);
+				if (child != requestedLock)
+					lock.children.add(locks.get(child));	
+			}
+		}
+		// Scan the graph for cycles
+		boolean hasCycle = false; 
+		for(LockNode n: locks){
+			if(n.hasCycle()){
+				hasCycle = true;
+				break;
+			}
+		}
+		// remove presale
+		if (!hasCycle){
+			for(int requestedLock: requestedLocks){
+				for(int child: requestedLocks){
+					LockNode lock = locks.get(requestedLock);
+					lock.owners.remove(owner);
+					if (child != requestedLock)				
+						lock.children.remove(locks.get(child));
+				}
+			}
+		}
+		request.unlock();
+		return hasCycle;
+		
+	}
+}
---
+++ src/concurrency/LockNode.java
@@ -0,0 +1,44 @@
+package concurrency;
+
+import java.util.ArrayList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockNode {
+	
+	enum Status {FRESH, VISITED}
+	
+	Status status = Status.FRESH;
+	ArrayList<LockNode> children = new ArrayList<LockNode>();
+	Lock lock = new ReentrantLock();
+	ArrayList<String> owners= new ArrayList<String>();
+	
+	public boolean hasCycle(){
+		return this.hasCycle(this);
+	}
+	
+	private boolean hasCycle(LockNode origin ){
+		status = status.VISITED;
+		for(LockNode n: children){
+			if (n.status == status.VISITED){
+				refresh();
+				return false;
+			}
+			else if(n != origin){
+				if(n.hasCycle(this))
+					refresh();
+					return true;
+			}
+		}
+		
+		refresh();
+		return false;
+	}
+	
+	private void refresh(){
+		for(LockNode n: children){
+			n.status = status.FRESH;
+		}
+		this.status  = status.FRESH;
+	}
+}
