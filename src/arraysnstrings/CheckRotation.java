2f21dd198cfdef9e09b8388b9d369ca8187d7888
Roberto Jimenez Sanchez <roberto@de.ibm.com>
2016-09-24 23:12:41 +0200

    Check Rotation added

--- src/arraysnstrings/CheckRotation.java
+++ src/arraysnstrings/CheckRotation.java
@@ -5,37 +5,8 @@ public class CheckRotation {
 	public static boolean isRotation(String s1, String s2){
 		if (s1.length() != s2.length())
 			return false;
-		
-		
-		int LENGTH = s1.length();
-		
-		if (LENGTH == 0)
-			return true; 
-		
-		StringBuilder x = new StringBuilder();
-		StringBuilder y = new StringBuilder();
-		boolean found = false; 
-		int i = 0;
-		int j = 0;
-		
-		while(j < LENGTH){
-			char c = s2.charAt(j);
-			
-			if (s1.charAt(i) == c){
-				found = true;
-				x.append(c);
-				i++;	
-			}else {
-				if(found){
-					found = false; 
-					y.append(x.chars());
-					x = new StringBuilder();
-				}
-				y.append(c);
-			}
-			j++; 
-		}
-		
-		return found ? true :s1.contains(y.toString()); 
+		// s1=xy & s2=yx & s1 + s1 = xyxy = x+ s2 + y
+		String s1s1= s1+s1;
+		return s1s1.contains(s2);
 	}
 }
