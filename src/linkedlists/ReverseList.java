43e9d262e063bf5f096df1758eb8f8d05950f8e3
Roberto Jimenez Sanchez <roberto@de.ibm.com>
2016-10-24 14:18:41 +0200

    Added reverse range

--- src/linkedlists/ReverseList.java
+++ src/linkedlists/ReverseList.java
@@ -7,12 +7,10 @@ public class ReverseList {
 		if (head.next == null)
 			return head;
 		
-		
 		Node runner = head; 
 		Node reverseHead = null;
 		
 		while (runner != null){
-			System.out.println("Runner:" + runner.data);
 			Node rest = runner.next; 
 			runner.next = reverseHead;
 			reverseHead = runner;
@@ -20,6 +18,42 @@ public class ReverseList {
 		}
 		
 		return reverseHead;
+	}
+	
+	
+	
+	public static Node reverseRange(Node head, int n, int m){
+		
+		
+		Node runner = head; 
+		
+		for(int i = 1; i < n ; i++ ){
+			runner = runner.next;
+		}
+		
+	
+		Node headTail = runner; 
+		System.out.println("Eliminate beginning. HeadTail: " + headTail.data);
+		
+		runner = runner.next; 
+		
+		Node reverseHead = runner; 
+		Node rest = runner.next; 
+		Node reverseTail = runner;
+		
+		for(int i = n; i < m ; i++ ){
+			rest = runner.next; 
+			runner.next = reverseHead; 
+			reverseHead = runner; 
+			runner = rest; 
+		}
+		
+		
+		reverseTail.next = rest; 
+		headTail.next = reverseHead; 
+		
+		return head;
+		
 	}
 
 }
--- src/linkedlists/ReverseListTest.java
+++ src/linkedlists/ReverseListTest.java
@@ -13,6 +13,30 @@ public class ReverseListTest {
 		head.append(0);
 	
 		Node result = ReverseList.reverse(head);
+
+		int i = 0;
+		
+		while (result != null){
+			assertEquals(i, result.data);
+			result = result.next;
+			i++;
+		}
+		
+		assertEquals(3,i);
+	}
+	
+	
+	
+	@Test
+	public void testReverseRange() {
+		Node head = new Node(0); 
+		head.append(1);
+		head.append(2);
+		head.append(5);
+		head.append(4);
+		head.append(3);
+	
+		Node result = ReverseList.reverseRange(head,3,6);
 		result.print();
 		
 		int i = 0;
@@ -23,7 +47,7 @@ public class ReverseListTest {
 			i++;
 		}
 		
-		assertEquals(3,i);
+		assertEquals(6,i);
 	}
 
 }
